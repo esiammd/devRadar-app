34f4a43710b7abb9ec34f965f1f2a638f83940cc
Maíse <esiammd@hotmail.com>
2020-01-17 14:23:16 -0300

    githubUsername
    githubUsarname -> githubUsername

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,10 +5,10 @@ import { WebView } from 'react-native-webview'
 
 function Profile({navigation}) {
 
-  const githubUsarname = navigation.getParam('github_username')
+  const githubUsername = navigation.getParam('github_username')
 
   return (
-    <WebView style={{flex: 1}} source={{uri: `https://github.com/${githubUsarname}`}}/>
+    <WebView style={{flex: 1}} source={{uri: `https://github.com/${githubUsername}`}}/>
   )
 }
 
